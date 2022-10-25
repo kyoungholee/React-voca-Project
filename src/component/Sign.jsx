21918b6e9f9568e014ce0340c8e0665cc14056a5
kyoungho lee <dlrudgh1303@gmail.com>
2022-10-26 02:00:46 +0900

    first commit

--- src/component/Sign.jsx
+++ src/component/Sign.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from '../Sign.module.css';
 
 export default function Sign() {
+    
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [birth, setBirth] = useState("");
+
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -18,7 +27,14 @@ export default function Sign() {
             <label>
                 아이디
             </label>
-            <input className={`${style.ID_input}`} type="text" placeholder='아이디 입력 ' />
+            <input className={`${style.ID_input}`} name = "userName" value={id} placeholder='아이디를 입력해주세요' onChange />
+        </div>
+
+        <div className={`${style.ID_start}`}>
+            <label>
+                이름
+            </label>
+            <input className={`${style.ID_input}`} name = "NickName" value={name} placeholder='이름을 입력해주세요' onChange />
         </div>
 
 
@@ -26,20 +42,20 @@ export default function Sign() {
             <label>
                 비밀번호
             </label>
-            <input className={`${style.PW_input}`} type="text" placeholder='비밀번호 입력' />
+            <input className={`${style.PW_input}`} name = "password" value={password} placeholder='비밀번호 입력' />
         </div>
         <div className={`${style.PW2_start}`}>
             <label>
                 비밀번호 확인
             </label>
-            <input className={`${style.PW2_input}`} type="text" placeholder='비밀번호 한번 더 입력' />
+            <input className={`${style.PW2_input}`} name = "password2" value={passwordConfirm} placeholder='비밀번호 한번 더 입력' />
         </div>
 
         <div className={`${style.EM_start}`}>
             <label>
                 이메일
             </label>
-            <input className={`${style.EM_input}`} type="text" placeholder='이메일 입력' />
+            <input className={`${style.EM_input}`} name = "email" value={email} placeholder='이메일 입력' />
             
         </div>
 
@@ -47,17 +63,17 @@ export default function Sign() {
             <label>
                 휴대폰 번호 
             </label>
-            <input className={`${style.PN_input}`} type="text" placeholder='숫자를 입력하세요' />
+            <input className={`${style.PN_input}`} name = "phone" value={phone} placeholder='숫자를 입력하세요' />
         </div>
 
         <div className={`${style.Sex_start}`}>
             <label >
-                성별 
+                생년월일 
             </label>
-            <input type="text" placeholder='성별을 입력해주세요' />
+            <input type="text" value={birth} placeholder='성별을 입력해주세요' />
         </div>
 
-        <button>가입하기</button>
+        <button className={`${style.join_btn}`}>가입하기</button>
         <button>돌아가기</button>
         
         </form>
